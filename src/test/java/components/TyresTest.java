7b24860bfd243add02d6ac09d7073202e0c91323
Emma Hack <ehack78@gmail.com>
2021-01-13 15:22:49 +0000

    tyres all tests passed

--- src/test/java/components/TyresTest.java
+++ src/test/java/components/TyresTest.java
@@ -1,4 +1,49 @@
 package components;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TyresTest {
+
+    Tyres tyres;
+
+    @Before
+    public void setUp(){
+        tyres = new Tyres(19, "Pirelli", "Summer");
+    }
+
+    @Test
+    public void canGetSizeOfTyres(){
+        assertEquals(19, tyres.getSize());
+    }
+
+    @Test
+    public void canChangeSizeOfTyres(){
+        tyres.changeSize(16);
+        assertEquals(16, tyres.getSize());
+    }
+
+    @Test
+    public void canGetBrandOfTyres(){
+        assertEquals("Pirelli", tyres.getBrand());
+    }
+
+    @Test
+    public void canChangeBrandOfTyres(){
+        tyres.changeBrand("Michellin");
+        assertEquals("Michellin", tyres.getBrand());
+    }
+
+    @Test
+    public void canGetSeasonOfTyres(){
+        assertEquals("Summer", tyres.getSeason());
+    }
+
+    @Test
+    public void canChangeSeasonOfTyres(){
+        tyres.changeSeason("Winter");
+        assertEquals("Winter", tyres.getSeason());
+    }
 }
